da40e68b4e77a61d7db209541bbcbce74f02f44e
Ewerthon Oliveira <ewerthonaraujo@outlook.com>
2023-08-06 14:35:02 -0300

    refactor: mudança nos botes brancos e pretos

--- src/components/button-black/index.jsx
+++ src/components/button-black/index.jsx
@@ -1,8 +1,13 @@
 import styles from "./styles.module.css"
 
+import { useContext } from "react"
+import { CartContext } from "../../contexts/cart"
+
 export const ButtonBlack = () => {
+  const {quant, setQuant} = useContext(CartContext)
+
   return (
-    <button className={styles.buttonBlack}>
+    <button className={styles.buttonBlack} onClick={()=>setQuant(quant+1)}>
       COMPRAR
     </button>
   )
