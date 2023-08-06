bf680f25a4f241b33b75605425d9df5e73075718
Ewerthon Oliveira <ewerthonaraujo@outlook.com>
2023-08-06 14:40:55 -0300

    refactor: app com todos os componentes da pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-// import { useEffect } from "react"
 import { Header } from "./components/header"
-
+import { CarrouselEcommerce } from "./components/carrousel-ecommerce"
+import { SectionProducts } from "./components/section-carrousel-products"
+import { FormNews } from "./components/imput-form"
+import { Footer } from "./components/footer"
 
 export const App = () => {
 
   return (
     <>
-      <Header/>
+      <Header />
+      <CarrouselEcommerce/>
+      <SectionProducts />
+      <FormNews/>
+      <Footer/>
     </>
   )
 }
