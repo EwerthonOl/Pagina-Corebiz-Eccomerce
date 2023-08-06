6dd57a81aed91f38aa36c4550cd5f6ba5a12efcf
Ewerthon Oliveira <ewerthonaraujo@outlook.com>
2023-08-06 14:35:47 -0300

    refactor: mudança no componente imagem inicial

--- src/components/carrousel-ecommerce/index.jsx
+++ src/components/carrousel-ecommerce/index.jsx
@@ -1,11 +1,28 @@
 import styles from "./styles.module.css" 
 
 import bannerCell from '../../assets/banner-cell.svg'
+import bannePc from "../../assets/banner.svg"
+
+import { useState, useEffect } from "react"
 
 export const CarrouselEcommerce = () => {
+  const [imgSrc, setImgSrc] = useState(bannerCell);
+
+  //USA A FUNÇAÕ DOS UTILS PARA DEFINIR O BANNER DA TELA INICIAL DE ACORDO COM A VIEW
+  useEffect(() => {
+    setImgSrc(window.innerWidth >= 520 ? bannePc : bannerCell);
+
+    window.addEventListener("resize", () => 
+      setImgSrc(window.innerWidth >= 520 ? bannePc : bannerCell));
+
+    return window.removeEventListener("resize", () =>
+      setImgSrc(window.innerWidth >= 520 ? bannePc : bannerCell)
+    );
+  }, []);
+
   return (
     <section className={styles.sectionCarrousel}>
-      <img className={styles.imgCarrousel} src={bannerCell} alt="" />
+      <img className={styles.imgCarrousel} src={imgSrc} alt="" />
     </section>
   )
 }
