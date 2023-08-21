9bffb7cfe45e11e73792d2733046cdaa4feb99b9
Ewerthon Oliveira <ewerthonaraujo@outlook.com>
2023-08-21 16:04:01 -0300

    fix: product cart prototypes and styles

--- src/components/product-card/index.jsx
+++ src/components/product-card/index.jsx
@@ -1,38 +1,45 @@
 import styles from './styles.module.css'
+import PropTypes from 'prop-types';
 
 import ReactStars from "react-rating-stars-component";
 
 import { ButtonBlack } from '../button-black';
 
-export const ProductCard = ({image, title, rating, price, category, description}) => {
+export const ProductCard = ({ image, title, rating, price, category, description }) => {
+
+  ProductCard.propTypes = {
+    image: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    rating: PropTypes.object.isRequired,
+    price: PropTypes.number.isRequired,
+    category: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  }
 
   //COMPONENTE
   return (
     //CARD DE CADA PRODUTO
-    <div className={styles.divCard}>
+    <section className={styles.divCard}>
       {/* IMAGEM DO PRODUTO E NOME */}
-      <img src={image} alt={description} title={title}/>
-      
-      <div className={styles.divDescriptProdc}>
-        <p className={styles.productNameP} title={title}>{title.slice(0,20)}</p>
-        <ReactStars 
+      <img src={image} alt={description} title={title} />
+
+      <article className={styles.divDescriptProdc}>
+        <p className={styles.productNameP} title={title}>{title.slice(0, 20)}</p>
+        <ReactStars
           count={5}
           value={rating.rate}
           isHalf={true}
-          activeColor="#F8475F" 
+          activeColor="#F8475F"
           a11y={false}
           edit={false}
-          emptyIcon={<i className="far fa-star"></i>}
-          halfIcon={<i className="fa fa-star-half-alt"></i>}
-          fullIcon={<i className="fa fa-star"></i>}
         />
 
-        <strong>por R$ {price}</strong>
+        <strong>por R$ {price.toLocaleString('PT')}</strong>
         <p className={styles.installmentsP}>{category}</p>
 
-        <ButtonBlack/>
-      </div>
-    </div>
+        <ButtonBlack />
+      </article>
+    </section>
   )
 
 }
